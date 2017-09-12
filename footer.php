2875e1870b54f03ad85e567d561e0f6647a73487
Abed Putra <abedputra@gmail.com>
2017-09-12 14:08:27 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+		<!-- Footer -->
+		<footer>
+			<div class="col-md-12" style="text-align:center;">
+                <hr>
+                Copyright - 2017 | <a href="http://abedputra.com">abedputra.com</a>
+            </div>
+		</footer>
+	</div>
+</body>
+</html>
