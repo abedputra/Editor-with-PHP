bab8fd94c1f5eb09303566bf8c350853749635c4
Abed Putra <abedputra@gmail.com>
2017-09-12 14:15:39 +0800

    Create file-edit.php

---
+++ file-edit.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Edit Text</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+	<!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery Scripts -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+<div class="container">
+<h1>testing</h1>
+<hr>
+<br>
+<p>Maecenas sollicitudin tristique risus in sagittis. Aenean pellentesque diam sit amet purus feugiat laoreet. Pellentesque id semper libero, non viverra magna. Aliquam condimentum nulla sit amet neque faucibus dictum. Donec non placerat libero. Nunc ultrices risus quis libero sollicitudin lacinia. Suspendisse libero nisl, viverra iaculis purus ac, blandit eleifend risus. Cras tempor erat ac egestas gravida. Proin vel pulvinar quam, eu dictum erat.</p>
+
+<p>Nulla sollicitudin erat quis sapien laoreet, sed luctus dolor pellentesque. Sed eget nisl massa. Etiam dictum scelerisque cursus. Sed vestibulum risus tincidunt nibh lobortis, nec elementum nulla egestas. Pellentesque sit amet dolor dapibus metus luctus fermentum at et purus. Proin non orci ante. Vivamus pharetra dolor vel ex imperdiet tristique. Curabitur elementum sem eget sem congue, non malesuada dolor sodales. Pellentesque pretium tincidunt elit eget varius. In viverra lectus ac dui eleifend, eu bibendum felis pretium. Ut venenatis nibh et magna lacinia, vel consequat nisi tincidunt.</p>
+
+<p>Curabitur non volutpat est. Pellentesque sapien urna, dapibus vitae tortor et, facilisis tincidunt nunc. Proin interdum justo eros. Nullam varius libero nec libero consequat, hendrerit gravida orci bibendum. Duis placerat sollicitudin interdum. In quis neque bibendum, vestibulum tortor ut, pretium justo. Integer convallis id lectus id vehicula. Donec vel enim sed tortor tincidunt dictum vel quis mi. Integer ultrices finibus ante, non pulvinar purus vestibulum quis.</p>
+
+<p>Cras dictum sapien felis, at convallis tortor finibus ac. In hac habitasse platea dictumst. Donec at nibh vitae diam vestibulum dapibus vel luctus ante. Donec neque massa, dictum suscipit sollicitudin et, fermentum a metus. Nam efficitur dapibus urna sed volutpat. Duis eget diam congue mauris malesuada accumsan nec ac diam. Ut pulvinar, lacus a fermentum tristique, risus metus blandit nunc, at accumsan risus diam sit amet nisi. Suspendisse lobortis facilisis elementum. Cras metus risus, lacinia ultricies tempor placerat, pretium eget turpis. Mauris tempor mollis feugiat.</p>
+</div>
+</body>
+</html>
