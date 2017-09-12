e07edde0bdc00040ac3f709ce06768b7a2e90ed7
Abed Putra <abedputra@gmail.com>
2017-09-12 14:07:57 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include once "header.php";
+?>
+<div class="container">
+        <div class="page-header">
+            <h1>Editor <small>for PHP</small></h1>
+        </div>
+        <h3>For file: <a href="http://abedputra.com/<?php echo $file; ?>" target="_blank"><?php echo $file; ?></a>
+        </h3>
+        <!-- HTML form -->
+        <form action="" method="post" class="form-group">
+            <div class="form-group">
+                <textarea name="text" id="editor"  rows="20" class="form-control"><?php echo htmlspecialchars($text) ?></textarea>
+            </div>
+            <div class="form-group">
+                  <input type="submit" class="btn btn-primary"/>
+              </div>
+        </form>
+<?php
+include once "footer.php";
+?>
